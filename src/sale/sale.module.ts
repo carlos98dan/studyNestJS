afb67f933b526b1d2550feec8a3a823bd822a2b3
Carolina Florez Romero <carolina.florez@ucp.edu.co>
2024-09-12 15:56:27 -0500

    add service

--- src/sale/sale.module.ts
+++ src/sale/sale.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SaleController } from './sale.controller';
+import { SaleService } from './sale.service';
 
 @Module({
-  controllers: [SaleController]
+  controllers: [SaleController],
+  providers: [SaleService]
 })
 export class SaleModule {}
