69191f09f369b4cededfe63eb68a1bca1e8e1949
Carolina Florez Romero <carolina.florez@ucp.edu.co>
2024-09-14 12:32:27 -0500

    create category with resource command

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
 import { ClientModule } from './client/client.module';
 import { SaleModule } from './sale/sale.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [ProductModule, ClientModule, SaleModule],
+  imports: [ProductModule, ClientModule, SaleModule, CategoryModule],
   controllers: [],
   providers: [],
 })
