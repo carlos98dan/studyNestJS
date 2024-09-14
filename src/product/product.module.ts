523178a5f4913b48ccb36670b16ec4904cbc2372
Carolina Florez Romero <carolina.florez@ucp.edu.co>
2024-09-14 12:23:15 -0500

    add other middleware for authorization

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -7,6 +7,7 @@ import {
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { AuthMiddleware } from './middlewares/auth.middleware';
 
 @Module({
   controllers: [ProductController],
@@ -15,15 +16,19 @@ import { LoggerMiddleware } from './middlewares/logger.middleware';
 export class ProductModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     // consumer.apply(LoggerMiddleware).forRoutes('product');
-    consumer.apply(LoggerMiddleware).forRoutes(
-      {
-        path: '/product',
-        method: RequestMethod.GET,
-      },
-      {
-        path: '/product',
-        method: RequestMethod.POST,
-      },
-    );
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(
+        {
+          path: '/product',
+          method: RequestMethod.GET,
+        },
+        {
+          path: '/product',
+          method: RequestMethod.POST,
+        },
+      )
+      .apply(AuthMiddleware)
+      .forRoutes('product');
   }
 }
