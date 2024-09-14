296c75ed742234f387d31bc3790a1415fbd103f0
Carolina Florez Romero <carolina.florez@ucp.edu.co>
2024-09-14 11:33:28 -0500

    Pipe on route

--- src/sale/sale.controller.ts
+++ src/sale/sale.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 
 @Controller('sale')
 export class SaleController {
   @Get()
   index() {
     return 'Pagina Inicial Ventas';
+  }
+
+  @Get('/ticket/:num')
+  getNumber(@Param('num', ParseIntPipe) num: number) {
+    return num + 14;
   }
 }
