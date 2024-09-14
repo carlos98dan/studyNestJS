289342f481fa5bdfc44c77acff60425e496b912c
Carolina Florez Romero <carolina.florez@ucp.edu.co>
2024-09-14 12:13:07 -0500

    Middleware impor level module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   controllers: [ProductController],
-  providers: [ProductService]
+  providers: [ProductService],
 })
-export class ProductModule {}
+export class ProductModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('product');
+  }
+}
